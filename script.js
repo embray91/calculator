fc3fd69fd1d4409d10e53e5396f83a4ce8b86d01
embray91 <embray91@gmail.com>
2022-01-11 09:21:20 -0600

    basic layout started, basic formulas started

---
+++ script.js
@@ -0,0 +1,25 @@
+const add = (a, b) => {
+let sum = a + b;
+return sum;
+};
+
+const subtract = (a, b) => {
+let diff = a - b;
+return diff;
+};
+
+const multiply = (e, f) => {
+let quot = (a * b);
+return quot;
+};
+
+const divide = (a, b) => {
+let diff = a/b;
+return diff;
+};
+
+const operate = (operator, num1, num2) => {
+a = num1;
+b = num2;
+return operator(a,b);
+};
