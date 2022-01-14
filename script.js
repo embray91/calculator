4cd46e2decdef59dc9fd26e269d32c6ea39728be
embray91 <embray91@gmail.com>
2022-01-13 22:26:23 -0600

    Added display function for buttons

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-const add = (a, b) => {
-let sum = a + b;
-return sum;
+
+let display =0;
+
+let operations = {
+add: function(a, b) {
+    display = a + b;
+    return display;
+    },
+
+subtract: function(a, b) {
+    display = a - b;
+    return display;
+    },
+
+multiply: function(a, b) {
+    display = (a * b);
+    return display;
+    },
+
+divide: function(a, b) {
+    display = a/b;
+    return display;
+    },
+
+operate: function(operator, num1, num2) {
+    a = num1;
+    b = num2;
+    return operator(a,b);
+    }
 };
 
-const subtract = (a, b) => {
-let diff = a - b;
-return diff;
-};
+let screen = document.querySelector('.screen');
 
-const multiply = (e, f) => {
-let quot = (a * b);
-return quot;
-};
+const buttons = document.getElementsByClassName('button');
 
-const divide = (a, b) => {
-let diff = a/b;
-return diff;
-};
+for (let button of buttons) {
+button.addEventListener('click', function(e) {
+let screenText = document.createTextNode(e.currentTarget.textContent);
+screen.appendChild(screenText);
+});
+}
 
-const operate = (operator, num1, num2) => {
-a = num1;
-b = num2;
-return operator(a,b);
-};
+const operators = document.querySelector('#operators');
+
+for (let op of operators.children) {
+    op.addEventListener('click', function(e) {
+        if () // ... left off here, need to figure out if operators can be props in obj?
+    });
+}
+
